6f1e0ccab284f0a1a4d10bb7a71e12779fc50c6a
Tim Cannady <timcannady@gmail.com>
2015-10-16 10:24:29 -0700

    add framework to todo model

---
+++ js/models/todo.js
@@ -0,0 +1,17 @@
+var app = app || {};
+
+// todo model
+app.Todo = Backbone.model.extend({
+  defaults: {
+    title: '',
+    completed: false
+  },
+
+  // toggle the completed status of the todo item
+  toggle: function(){
+    // when toggle() is triggered, trigger save() on the model passing in the opposite of it's current completed status
+    this.save({
+      completed: !this.get('completed')
+    })
+  }
+})
