fa01e8365dfe07a630aa21262f89a31d2082d6e6
Tim Cannady <timcannady@gmail.com>
2015-10-16 14:12:47 -0700

    add render function to app collection

--- js/views/app.js
+++ js/views/app.js
@@ -13,6 +13,13 @@ app.AppView = Backbone.View.extend({
   // our template for the line of statistics at the bottom of the app
   statsTemplate: _.template($('#stats-template').html()),
 
+  // delegated events for creating new items and for clearing completed ones
+  events: {
+    'keypress #new-todo': 'createOnEnter',
+    'click #clear-completed': 'clearCompleted',
+    'click #toggle-all': 'toggleAllComplete'
+  },
+
   // when initialized, bind the relevant events on the 'todos' collection, when items are added or changed. What exactly is this doing?
   initialize: function(){
     this.allCheckbox = this.$('#toggle-all')[0];
@@ -24,7 +31,7 @@ app.AppView = Backbone.View.extend({
     this.$footer = this.$('#footer');
     this.$main = this.$('#main');
 
-    // two event listeners. note that we're only binding add and reset to the collection (app). We're going to delegate update and delete to the TodoView (model)
+    // two+ event listeners. note that we're only binding add and reset to the collection (app). We're going to delegate update and delete to the TodoView (model)
 
     // is this a replacement for something like
     // this.collection.on('add', this.addOne, this) ??
@@ -32,6 +39,44 @@ app.AppView = Backbone.View.extend({
     // answer: listenTo() sets the callback's context to the view when it creates the binding
     this.listenTo(app.Todos, 'add', this.addOne);
     this.listenTo(app.Todos, 'reset', this.addAll);
+
+    this.listenTo(app.Todos, 'change:completed', this.addOne);
+    this.listenTo(app.Todos, 'filter', this.filterAll);
+    this.listenTo(app.Todos, 'all', this.render);
+
+    // load any preexisting todos (in our case from localStorage)
+    app.Todos.fetch()
+  },
+
+  // re-rendering the App just means refreshing the statistics - the rest of the app doesn't change
+  render: function(){
+    // these work because we did fetch() on initialize?
+    // And I'm not sure why it's not something more like
+    // var completed = app.Todos.get('completed').length
+    // Answer: completed() and remaining() are functions we defined in the Collection class. OOP yo! Don't forget I'm in the view. The model (collection) should have the logic, eg the functions to get the data.
+    var completed = app.Todos.completed().length;
+    var remaining = app.Todos.remaining().length;
+
+    // only display the list and filtering box if there are todos in the system
+    if( app.Todos.length ){
+      this.$main.show();
+      this.$footer.show();
+
+      this.$footer.html(this.statsTemplate({
+        completed: completed,
+        remaining: remaining
+      }));
+
+      this.$('#filters li a')
+        .removeClass('selected')
+        .filter('[href="#/' + ( app.todoFilter || '') + '"]')
+        .addClass('selected');
+    } else{
+      this.$main.hide();
+      this.$footer.hide()
+    }
+
+    this.allCheckbox.checked = !remaining;
   },
 
   // add a single todo item to the list by creating a view for it, and appending its element to the #todo-list <ul>. How does this know to create a <li>? Maybe because append knows..
